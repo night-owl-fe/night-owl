7a2ee11dbfea6c22086a37d7ffca47db3d0280dd
陈俊汝 <chenjunru05@gmail.com>
2017-11-20 22:31:07 +0800

    Add lodash assignIn

--- modules/lodash/assign.js
+++ modules/lodash/assign.js
@@ -42,15 +42,19 @@ function Bar () {
 }
 Foo.prototype.d = 3
 Bar.prototype.e = 3
-let b = Symbol('bb')
-Bar.prototype[b] = 3
-const obj = {}
-// const res = assign(obj, new Foo, new Bar)
-// console.log('array----', assign({a: 1}, [{b: 2}], {a: 2, d: 3}))
-// console.log('string---', assign({a: 1}, 'text', {a: 2, d: 3}))
-// console.log('number---', assign({a: 1}, 123, {a: 2, d: 3}))
-// console.log('null', assign({}, null, {a: 1, d: 3}))
-let s = Symbol('ss')
-let a = Symbol('aa')
-let d = Symbol('dd')
-console.log('Symbol----', assign({t: 1}, new Bar, {[s]: 2}, {[a]: 2, [d]: 3, c: 5})) //getOwnPropertySymbols
+
+console.log('正常情况-----', assign({}, new Foo, new Bar))
+console.log('存在数组的情况-----', assign({a: 1}, [{b: 2}], {a: 2, d: 3}))
+console.log('存在数组的情况-----', assign([{a: 1}], [{b: 2}], {a: 2, d: 3}))
+console.log('存在字符串的情况-----', assign({a: 1}, 'text', {a: 2, d: 3}))
+console.log('存在数字的情况-----', assign({a: 1}, 123, {a: 2, d: 3}))
+console.log('存在null的情况-----', assign(null, {a: 2, d: 3}))
+
+
+let testSymbol = Symbol('Symbol')
+Bar.prototype[testSymbol] = 'SymbolPro'
+let testSymbol1 = Symbol('Symbol1')
+let testSymbol2 = Symbol('Symbol2')
+let testSymbol3 = Symbol('Symbol3')
+let testSymbol4 = Symbol('Symbol4')
+console.log('存在ES6语法Symbol的情况-----', assign({[testSymbol4]: 'Symbol4'}, new Bar, {[testSymbol3]: 'Symbol3'}, {[testSymbol1]: 'Symbol1', [testSymbol2]: 'Symbol2', c: 5}))
