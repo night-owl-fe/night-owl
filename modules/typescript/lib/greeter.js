4052bad47212ecf3c67d8dc28ee510f1a92bd2a9
wanglei8381 <wanglei8381@163.com>
2018-01-04 20:15:44 +0800

    学习typescript

---
+++ modules/typescript/lib/greeter.js
@@ -0,0 +1,7 @@
+"use strict";
+function greeter(person) {
+    return "Hello, " + person;
+}
+let user = "Jane User";
+let re = greeter(1);
+console.log(re);
---
+++ modules/typescript/src/greeter.ts
@@ -0,0 +1,9 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+let re = greeter(user);
+
+console.log(re);
